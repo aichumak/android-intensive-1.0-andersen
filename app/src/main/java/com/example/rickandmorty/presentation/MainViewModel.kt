03d4c846b8e946342c05894964fa49885bb05bbf
aichumak <aichumak.dev@gmail.com>
2022-05-14 21:22:41 +0300

    feature/add-fragments-navigation - add episode fragment navigation - add location fragment navigation - add character fragment navigation

--- app/src/main/java/com/example/rickandmorty/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/MainViewModel.kt
@@ -1,17 +1,11 @@
 package com.example.rickandmorty.presentation
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
-import com.example.rickandmorty.data.CharactersRepositoryImpl
-import com.example.rickandmorty.data.EpisodesRepositoryImpl
-import com.example.rickandmorty.data.LocationsRepositoryImpl
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 //    val charactersRepository = CharactersRepositoryImpl(context)
 //    val locationsRepository = LocationsRepositoryImpl(context)
 //    val episodesRepository = EpisodesRepositoryImpl(context)
-
-
 
 
 }
